1aeaa27fa9b872844ae95d8ba615a427cfbcfef1
Hans Becker <hansbecker91@gmail.com>
2017-02-20 11:31:04 +0100

    Added first prototype, that reads filename from running mpv instance and uses guessit for parsing

---
+++ mpv-trakt-daemon.py
@@ -0,0 +1,20 @@
+from guessit import guessit
+import json
+
+
+def main():
+    f = open(r'\\.\pipe\mpv', 'r+b')
+    # why r+b? http://stackoverflow.com/a/30566011/2634932
+    print("opened mpv pipe")
+
+    f.write(str.encode('{ "command": ["get_property", "filename"] }\n'))
+    filename = json.loads(f.readline())
+    print(guessit(filename["data"]))
+
+    while True:
+        pipe_line = f.readline()
+        print(pipe_line)
+
+
+if __name__ == '__main__':
+    main()
