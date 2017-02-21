57dd14f71a19514940cecc93e321aeac1d6e3e61
Hans Becker <hansbecker91@gmail.com>
2017-02-21 18:59:54 +0100

    Started with proper asynchronous pipe input handling (Goal: Read as fast as possible from mpv_pipe. No synchronous processing after reading) No longer crashes, when mpv sends a event after a command was issued

--- mpv-trakt-daemon.py
+++ mpv-trakt-daemon.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os
+from queue import Queue
+from threading import Lock
 from time import sleep
 from guessit import guessit
 import json
@@ -26,6 +28,9 @@ def main():
 
 
 def start_mpv_monitoring(pipe_path):
+    command_queue = Queue()
+    lock = Lock()
+
     mpv_pipe = None
     while mpv_pipe is None:
         try:
@@ -40,17 +45,7 @@ def start_mpv_monitoring(pipe_path):
 
     print("opened mpv pipe")
 
-    mpv_pipe.write(str.encode('{ "command": ["get_property", "path"] }\n'))
-    path = json.loads(mpv_pipe.readline())
-    print(guessit(path["data"]))
-
-    mpv_pipe.write(str.encode('{ "command": ["get_property", "percent-pos"] }\n'))
-    playback_position = json.loads(mpv_pipe.readline())
-    print(playback_position)
-
-    mpv_pipe.write(str.encode('{ "command": ["get_property", "pause"] }\n'))
-    pause = json.loads(mpv_pipe.readline())
-    print(pause)
+    issue_scrobble_commands(mpv_pipe, lock, command_queue)
 
     while True:
         line = mpv_pipe.readline()
@@ -61,14 +56,56 @@ def start_mpv_monitoring(pipe_path):
         mpv_json = json.loads(line)
         print(mpv_json)
         if 'event' in mpv_json:
-            event_name = mpv_json['event']
-            if event_name == 'pause':
-                pass
-                # TODO
-            elif event_name == 'unpause' or event_name == 'seek' or event_name == 'start-file':
-                mpv_pipe.write(str.encode('{ "command": ["get_property", "percent-pos"] }\n'))
-                playback_position = json.loads(mpv_pipe.readline())
-                print(playback_position)
+            handle_event(mpv_pipe, lock, command_queue, mpv_json)
+        elif 'data' in mpv_json:
+            handle_command_response(mpv_pipe, lock, command_queue, mpv_json)
+        else:
+            print('Unknown mpv output: ' + mpv_json)
+
+
+last_pause_state = last_playback_position = last_path = None
+
+
+def handle_command_response(mpv_pipe, lock, command_queue, response):
+    global last_pause_state, last_playback_position, last_path
+
+    last_command_elements = command_queue.get()['command']
+    if last_command_elements[0] == 'get_property':
+        if last_command_elements[1] == 'pause':
+            last_pause_state = response['data']
+        elif last_command_elements[1] == 'percent-pos':
+            last_playback_position = response['data']
+        elif last_command_elements[1] == 'path':
+            last_path = response['data']
+        if last_pause_state is not None and last_playback_position is not None and last_path is not None:
+            print('got everything for a scrobble', last_pause_state, last_playback_position, last_path)
+            last_pause_state = None
+            last_playback_position = None
+            last_path = None
+
+
+def handle_event(mpv_pipe, lock, command_queue, event):
+    event_name = event['event']
+    if event_name == 'pause' or event_name == 'unpause' or event_name == 'seek' or event_name == 'start-file':
+        issue_scrobble_commands(mpv_pipe, lock, command_queue)
+
+
+def issue_scrobble_commands(mpv_pipe, lock, command_queue):
+    issue_command_get_property(mpv_pipe, lock, command_queue, 'path')
+    issue_command_get_property(mpv_pipe, lock, command_queue, 'percent-pos')
+    issue_command_get_property(mpv_pipe, lock, command_queue, 'pause')
+
+
+def issue_command(mpv_pipe, lock, command_queue, elements):
+    command = {'command': elements}
+    with lock:
+        mpv_pipe.write(bytes(json.dumps(command), 'utf-8'))
+        mpv_pipe.write(str.encode('\n'))
+        command_queue.put(command)
+
+
+def issue_command_get_property(mpv_pipe, lock, command_queue, property_name):
+    issue_command(mpv_pipe, lock, command_queue, ['get_property', property_name])
 
 
 if __name__ == '__main__':
