c6fab126bc4f19391f1dfdd519e62d9736c393a1
Hans Becker <hansbecker91@gmail.com>
2017-02-21 10:22:23 +0100

    Updated for device oauth authentication and used new urls and ids

--- trakt_v2_oauth.py
+++ trakt_v2_oauth.py
@@ -1,6 +1,5 @@
-#! /usr/bin/env python3
-
 import datetime
+import time
 import json
 import os
 import sys
@@ -9,42 +8,61 @@ import requests
 
 import client_secret_holder
 
-client_id = '18313245490a6414e3f46e981e263f845d28c716cd24a6c58137e89a869dfdcb'
-local_storage_json_file = './imdb_to_trakt_v2_sync_token.json'
+client_id = '24c7a86d0a55334a9575734decac760cea679877fcb60b0983cbe45996242dd7'
+local_storage_json_file = './trakt_token.json'
 
 
-def get_access_token(oauth_pin=None):
-    if not os.path.isfile(local_storage_json_file) and oauth_pin is None:
-        # no local token was found and no pin was supplied
-        sys.exit('Please supply an OAuth trakt pin. See -h for instructions.')
+def prompt_device_authentication():
+    code_request = requests.post('https://api.trakt.tv/oauth/device/code', json={
+        'client_id': client_id
+    })
 
-    if oauth_pin is not None:
-        # every time a PIN is supplied, try getting a new token
-        token_request = requests.post('https://api-v2launch.trakt.tv/oauth/token', json={
-            'code': oauth_pin,
-            'client_id': client_id,
-            'client_secret': client_secret_holder.get(),
-            'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob',
-            'grant_type': 'authorization_code'
-        })
+    if code_request.status_code == 200:
+        code_json = code_request.json()
+        print('Please visit %s and enter code %s to grant this app permission to your trakt account.' %
+              (code_json['verification_url'], code_json['user_code']))
+        start_time = datetime.datetime.now()
+        got_access_token = False
 
-        if token_request.status_code == 200:
-            print('Successfully got token from supplied PIN.')
-            # save response to local json file
-            json.dump(token_request.json(), open(local_storage_json_file, 'w'))
-        else:
-            sys.exit('Exchanging code for token failed with http code %d.\n%s' %
-                     (token_request.status_code, token_request.text))
+        while datetime.datetime.now() - start_time < datetime.timedelta(seconds=code_json['expires_in']):
+            time.sleep(code_json['interval'])
+            token_request = requests.post('https://api.trakt.tv/oauth/device/token', json={
+                'code': code_json['device_code'],
+                'client_id': client_id,
+                'client_secret': client_secret_holder.get()
+            })
+            if token_request.status_code == 200:
+                token_json = token_request.json()
+                # add created_at date to token_json, because it got removed from the trakt api
+                token_json['created_at'] = int(datetime.datetime.utcnow().timestamp())
+                json.dump(token_json, open(local_storage_json_file, 'w'))
+                print('\nSuccessfully established access to trakt account')
+                got_access_token = True
+                break
+            else:
+                print(str(token_request.status_code) + ' ', end='', flush=True)
+
+        if not got_access_token:
+            sys.exit('\nCould not get access token. Please try again.')
+
+    else:
+        sys.exit('POST request for generating device codes failed with HTTP code %d.\n%s' %
+                 (code_request.status_code, code_request.text))
+
+
+def get_access_token():
+    if not os.path.isfile(local_storage_json_file):
+        prompt_device_authentication()
 
     tokens = json.load(open(local_storage_json_file))
 
     expire_date = datetime.datetime.utcfromtimestamp(tokens['created_at'] + tokens['expires_in'])
     remaining_time = expire_date - datetime.datetime.utcnow()
 
-    # make sure the token is at least valid for the next 60 seconds (should be enough runtime for the script)
-    if remaining_time < datetime.timedelta(seconds=60):
+    # make sure the token is at least valid for the next day
+    if remaining_time < datetime.timedelta(days=1):
         print('Token expired')
-        token_refresh_request = requests.post('https://api-v2launch.trakt.tv/oauth/token', json={
+        token_refresh_request = requests.post('https://api.trakt.tv/oauth/token', json={
             'refresh_token': tokens['refresh_token'],
             'client_id': client_id,
             'client_secret': client_secret_holder.get(),
