e50597800fed51cc93773aba1927a3dabea6b08e
Hans Becker <hansbecker91@gmail.com>
2017-03-30 15:31:06 +0200

    Now requesting mpv state regularly
    Because no data can be requested, when mpv is terminating (end-file event). So the last known data has to be used to do necessary scrobble

--- mpv-trakt-daemon.py
+++ mpv-trakt-daemon.py
@@ -16,7 +16,8 @@ MPV_WINDOWS_NAMED_PIPE_PATH = r'\\.\pipe\mpv'
 MPV_POSIX_SOCKET_PATH = '/tmp/mpv-socket'
 
 SECONDS_BETWEEN_MPV_RUNNING_CHECKS = 5.0
-SECONDS_BETWEEN_MPV_EVENT_AND_TRAKT_SYNC = 1.0
+SECONDS_BETWEEN_MPV_EVENT_AND_TRAKT_SYNC = 2.0
+SECONDS_BETWEEN_REGULAR_GET_PROPERTY_COMMANDS = 10.0
 FACTOR_MUST_WATCH_BEFORE_SCROBBLE = 0.1
 PERCENT_MINIMAL_PLAYBACK_POSITION_BEFORE_SCROBBLE = 90.0
 
@@ -29,12 +30,16 @@ last_path = None
 last_duration = None
 last_file_start_timestamp = None
 
+is_local_state_dirty = True
+
 next_sync_timer = None
+next_regular_timer = None
 
 
 def on_command_response(monitor, command, response):
     global last_is_paused, last_playback_position, last_path, last_duration, last_file_start_timestamp
-    global next_sync_timer
+    global next_sync_timer, next_regular_timer
+    global is_local_state_dirty
 
     last_command_elements = command['command']
     if last_command_elements[0] == 'get_property':
@@ -48,17 +53,18 @@ def on_command_response(monitor, command, response):
             last_path = response['data']
         elif last_command_elements[1] == 'duration':
             last_duration = response['data']
-            print(last_duration, last_duration * FACTOR_MUST_WATCH_BEFORE_SCROBBLE)
         if last_is_paused is not None \
                 and last_playback_position is not None \
                 and last_path is not None \
                 and last_duration is not None:
-            if next_sync_timer is not None:
-                next_sync_timer.cancel()
-            next_sync_timer = threading.Timer(SECONDS_BETWEEN_MPV_EVENT_AND_TRAKT_SYNC, sync_to_trakt,
-                                              (last_is_paused, last_playback_position, last_path, last_duration,
-                                               last_file_start_timestamp))
-            next_sync_timer.start()
+            if is_local_state_dirty:
+                if next_sync_timer is not None:
+                    next_sync_timer.cancel()
+                next_sync_timer = threading.Timer(SECONDS_BETWEEN_MPV_EVENT_AND_TRAKT_SYNC, sync_to_trakt,
+                                                  (last_is_paused, last_playback_position, last_path, last_duration,
+                                                   last_file_start_timestamp))
+                next_sync_timer.start()
+
             last_is_paused = None
             last_playback_position = None
             last_path = None
@@ -67,16 +73,34 @@ def on_command_response(monitor, command, response):
 
 def on_event(monitor, event):
     event_name = event['event']
-    if event_name == 'pause' or event_name == 'unpause' or event_name == 'seek' or event_name == 'start-file':
+
+    # when a new file starts, acts as a new mpv instance got connected
+    if event_name == 'start-file':
+        on_disconnected()
+        on_connected(monitor)
+
+    if event_name == 'pause' or event_name == 'unpause' or event_name == 'seek':
+        global is_local_state_dirty
+        is_local_state_dirty = True
         issue_scrobble_commands(monitor)
+
+
+def on_connected(monitor):
+    issue_scrobble_commands(monitor)
 
 
 def on_disconnected():
     global last_file_start_timestamp
     last_file_start_timestamp = None
 
+    global is_local_state_dirty
+    is_local_state_dirty = True
+
     if next_sync_timer is not None:
         next_sync_timer.cancel()
+
+    if next_regular_timer is not None:
+        next_regular_timer.cancel()
 
 
 def issue_scrobble_commands(monitor):
@@ -84,6 +108,16 @@ def issue_scrobble_commands(monitor):
     monitor.send_get_property_command('percent-pos')
     monitor.send_get_property_command('pause')
     monitor.send_get_property_command('duration')
+    schedule_regular_timer(monitor)
+
+
+def schedule_regular_timer(monitor):
+    global next_regular_timer
+    if next_regular_timer is not None:
+        next_regular_timer.cancel()
+    next_regular_timer = threading.Timer(SECONDS_BETWEEN_REGULAR_GET_PROPERTY_COMMANDS, issue_scrobble_commands,
+                                         [monitor])
+    next_regular_timer.start()
 
 
 def get_watch_state(is_paused, playback_position, duration, start_time):
@@ -103,7 +137,9 @@ def get_watch_state(is_paused, playback_position, duration, start_time):
 
 
 def sync_to_trakt(is_paused, playback_position, path, duration, start_time):
-    print(get_watch_state(is_paused, playback_position, duration, start_time))
+    print('sync_to_trakt', get_watch_state(is_paused, playback_position, duration, start_time))
+
+    global is_local_state_dirty
 
     for monitored_directory in monitored_directories:
         if path.startswith(monitored_directory):
@@ -121,6 +157,7 @@ def sync_to_trakt(is_paused, playback_position, path, duration, start_time):
 
             if guess['type'] == 'episode':
                 if guess['title'].lower() not in id_cache['shows']:
+                    print('requesting trakt id for show', guess['title'])
                     req = requests.get('https://api.trakt.tv/search/show?field=title&query=%s' % guess['title'],
                                        headers={'trakt-api-version': '2', 'trakt-api-key': TRAKT_CLIENT_ID})
                     if req.status_code == 200 and len(req.json()) > 0:
@@ -128,7 +165,9 @@ def sync_to_trakt(is_paused, playback_position, path, duration, start_time):
                     else:
                         print('trakt request failed or unknown show', guess)
                 print('scrobbling show', id_cache['shows'][guess['title'].lower()])
+                is_local_state_dirty = False
             elif guess['type'] == 'movie':
+                print('requesting trakt id for movie', guess['title'])
                 if guess['title'].lower() not in id_cache['movies']:
                     req = requests.get('https://api.trakt.tv/search/movie?field=title&query=%s' % guess['title'],
                                        headers={'trakt-api-version': '2', 'trakt-api-key': TRAKT_CLIENT_ID})
@@ -137,6 +176,7 @@ def sync_to_trakt(is_paused, playback_position, path, duration, start_time):
                     else:
                         print('trakt request failed or unknown movie', guess)
                 print('scrobbling movie', id_cache['movies'][guess['title'].lower()])
+                is_local_state_dirty = False
             else:
                 print('Unknown guessit type', guess['type'])
 
@@ -148,8 +188,8 @@ def sync_to_trakt(is_paused, playback_position, path, duration, start_time):
 
 
 def main():
-    monitor = mpv.MpvMonitor.create(MPV_POSIX_SOCKET_PATH, MPV_WINDOWS_NAMED_PIPE_PATH, issue_scrobble_commands,
-                                    on_event, on_command_response, on_disconnected)
+    monitor = mpv.MpvMonitor.create(MPV_POSIX_SOCKET_PATH, MPV_WINDOWS_NAMED_PIPE_PATH,
+                                    on_connected, on_event, on_command_response, on_disconnected)
     while True:
         if monitor.can_open():
             monitor.run()
