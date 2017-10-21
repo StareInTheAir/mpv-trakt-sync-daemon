f036f0951b11518ca3d72cc312c34bf6736d3b68
Hans Becker <hansbecker91@gmail.com>
2017-10-21 11:57:29 +0200

    Set version to 1.0.2

--- sync_daemon.py
+++ sync_daemon.py
@@ -189,7 +189,7 @@ def sync_to_trakt(is_paused, playback_position, working_dir, path, duration, sta
 
         if data is not None:
             data['progress'] = playback_position
-            data['app_version'] = '1.0.1'
+            data['app_version'] = '1.0.2'
 
             finished = is_finished(playback_position, duration, start_time)
 
